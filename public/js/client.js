ab7443bad9c4dd5dbc6cd37d2fc1bfa4dbd1cf68
Daniel Do <dodaniel006@gmail.com>
2025-03-12 12:05:11 -0700

    Ajax and DOM testing version

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,38 @@
 console.log("client.js loaded.");
 
-function sidebarHover() {
-    alert("hello");
+function ajaxGET(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            //console.log('responseText:' + xhr.responseText);
+            callback(this.responseText);
+        } else {
+            console.log(this.status);
+        }
+    }
+    xhr.open("GET", url);
+    xhr.send();
 }
 
-document.getElementById("sidebar").addEventListener("mouseover" , sidebarHover);
+document.querySelector("#weekdaysHTML").addEventListener("click", function (e) {
+    ajaxGET("/weekdays?format=json", function (data) {
+        console.log("Hello there");
+        // since it's HTML, let's drop it right in
+        document.getElementById("weekdaysText").innerHTML = data;
+    });
+});
+
+document.querySelector("#weekdaysJSON").addEventListener("click", function (e) {
+    ajaxGET("/weekdays?format=json", function (data) {
+        console.log("Before parsing", data);
+        // this call is JSON so we have to parse it:
+        let parsedData = JSON.parse(data);
+        console.log("After parsing", parsedData);
+        let str = "<ol>"
+        for (let i = 0; i < parsedData.length; i++) {
+            str += "<li>" + parsedData[i] + "</li>";
+        }
+        str += "</ol>";
+        document.getElementById("weekdaysJSON").innerHTML = str;
+    });
+});
